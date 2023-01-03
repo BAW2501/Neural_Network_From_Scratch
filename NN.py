9da272a7ac2580f4f73754ff737fb9264bb8370d
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-01-03 17:37:57 +0100

    full forward

--- NN.py
+++ NN.py
@@ -38,12 +38,30 @@ def relu_backward(dA, Z):
 
 def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
     Z_curr = np.dot(W_curr, A_prev) + b_curr
-    
-    if activation is "relu":
+
+    if activation == "relu":
         activation_func = relu
-    elif activation is "sigmoid":
+    elif activation == "sigmoid":
         activation_func = sigmoid
     else:
         raise Exception('Non-supported activation function')
-        
+
     return activation_func(Z_curr), Z_curr
+
+def full_forward_propagation(X, params_values, nn_architecture):
+    memory = {}
+    A_curr = X
+
+    for idx, layer in enumerate(nn_architecture):
+        layer_idx = idx + 1
+        A_prev = A_curr
+
+        activ_function_curr = layer["activation"]
+        W_curr = params_values[f"W{str(layer_idx)}"]
+        b_curr = params_values[f"b{str(layer_idx)}"]
+        A_curr, Z_curr = single_layer_forward_propagation(A_prev, W_curr, b_curr, activ_function_curr)
+
+        memory[f"A{str(idx)}"] = A_prev
+        memory[f"Z{str(layer_idx)}"] = Z_curr
+
+    return A_curr, memory
