a416ee57b54077fdd9428d3099c4088c433a2426
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-01-03 17:33:30 +0100

    layer init

--- NN.py
+++ NN.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 nn_architecture = [
     {"input_dim": 2, "output_dim": 4, "activation": "relu"},
     {"input_dim": 4, "output_dim": 6, "activation": "relu"},
@@ -5,3 +7,17 @@ nn_architecture = [
     {"input_dim": 6, "output_dim": 4, "activation": "relu"},
     {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"},
 ]
+
+def init_layers(nn_architecture, seed = 99):
+    np.random.seed(seed)
+    number_of_layers = len(nn_architecture)
+    params_values = {}
+
+    for idx, layer in enumerate(nn_architecture):
+        layer_idx = idx + 1
+        layer_input_size = layer["input_dim"]
+        layer_output_size = layer["output_dim"]
+        params_values[f'W{str(layer_idx)}'] = (np.random.randn(layer_output_size, layer_input_size) * 0.1)
+        params_values[f'b{str(layer_idx)}'] = (np.random.randn(layer_output_size, 1) * 0.1)
+
+    return params_values
