8aa42808226b976cabba71abc0b21298a274933d
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-01-03 17:32:12 +0100

    defined model

---
+++ NN.py
@@ -0,0 +1,7 @@
+nn_architecture = [
+    {"input_dim": 2, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 6, "activation": "relu"},
+    {"input_dim": 6, "output_dim": 4, "activation": "relu"},
+    {"input_dim": 4, "output_dim": 1, "activation": "sigmoid"},
+]
