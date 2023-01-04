25ad511925d28b70474dd98547df67f88c3252ef
Belhadj Ahmed Walid <belhadj.ahmedwalid1@gmail.com>
2023-01-04 16:06:23 +0100

    working with mnist and ahcd

--- NN.py
+++ NN.py
@@ -1,4 +1,4 @@
-from data import get_mnist
+from data import get_mnist, get_ahcd
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,14 +8,15 @@ w = weights, b = bias, i = input, h = hidden, o = output, l = label
 e.g. w_i_h = weights from input layer to hidden layer
 """
 images, labels = get_mnist()
-w_i_h = np.random.uniform(-0.5, 0.5, (20, 784))
-w_h_o = np.random.uniform(-0.5, 0.5, (10, 20))
+n_features,n_classes = images.shape[1], labels.shape[1]
+w_i_h = np.random.uniform(-0.5, 0.5, (20, n_features))
+w_h_o = np.random.uniform(-0.5, 0.5, (n_classes, 20))
 b_i_h = np.zeros((20, 1))
-b_h_o = np.zeros((10, 1))
+b_h_o = np.zeros((n_classes, 1))
 
 learn_rate = 0.01
 nr_correct = 0
-epochs = 3
+epochs = 10
 for _ in range(epochs):
     for img, l in zip(images, labels):
         img.shape += (1,)
@@ -48,11 +49,12 @@ for _ in range(epochs):
 while True:
     index = int(input("Enter a number (0 - 59999): "))
     img = images[index]
-    plt.imshow(img.reshape(28, 28), cmap="Greys")
+    shape = int(np.sqrt(n_features)), int(np.sqrt(n_features))
+    plt.imshow(img.reshape(shape), cmap="Greys")
 
     img.shape += (1,)
     # Forward propagation input -> hidden
-    h_pre = b_i_h + w_i_h @ img.reshape(784, 1)
+    h_pre = b_i_h + w_i_h @ img.reshape(n_features, 1)
     h = 1 / (1 + np.exp(-h_pre))
     # Forward propagation hidden -> output
     o_pre = b_h_o + w_h_o @ h
--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 def get_mnist():
@@ -8,4 +9,18 @@ def get_mnist():
     images = images.astype("float32") / 255
     images = np.reshape(images, (images.shape[0], images.shape[1] * images.shape[2]))
     labels = np.eye(10)[labels]
+    return images, labels
+
+def get_images(folder):
+    return np.array([plt.imread(file).flatten() for file in folder.iterdir()])
+
+def get_labels(folder):
+    return np.array([int(file.stem.split('_')[-1]) for file in folder.iterdir()]) - 1
+
+def load_data(path):
+    return get_images(path), get_labels(path)
+
+def get_ahcd():
+    images, labels = load_data(Path('./ahcd'))
+    labels = np.eye(28)[labels]
     return images, labels
